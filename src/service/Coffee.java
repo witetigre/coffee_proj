c1ff11a49ce6c46e37ef3b3ae07be473eea8d976
witetigre <37624530+witetigre@users.noreply.github.com>
2018-12-06 15:51:21 +0200

    addCoffee

---
+++ src/service/Coffee.java
@@ -0,0 +1,45 @@
+public abstract class Coffee
+{
+
+
+    public abstract boolean loadCoffee(String packedCoffee); // Должен реализовывать
+    
+
+
+
+
+}
+class CoffeeBeans extends Coffee{
+
+  private String coffeeType;
+  private String packageType;
+  private float vol; // Обьем
+  private float cost;
+
+  public boolean loadCoffee(String packageCoffee){
+
+      //Записываем кофе в файл фургона
+       return true;
+    }
+}
+class CoffeeGround extends Coffee{
+
+    private String coffeeType;
+    private String packageType;
+    private float vol; // Обьем
+    private float cost;
+
+    public boolean loadCoffee(String packageCoffee){
+        return true;
+    }
+}
+class CoffeeInstant extends Coffee{
+    private String coffeeType;
+    private String packageType;
+    private float vol; // Обьем
+    private float cost;
+
+    public boolean loadCoffee(String packageCoffee){
+        return true;
+    }
+}
